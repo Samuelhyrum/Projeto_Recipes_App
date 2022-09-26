a32410361953ac7f3f575e948cbfde5015308d76
Bruno Varsan <brunovarsan09@hotmail.com>
2022-09-26 17:19:50 -0300

    Fix Footer.css and add <header /> in Drinks.jsx

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function Drinks() {
   return (
     <div>
-      Drinks
+      <Header title="Drinks" />
       <Footer />
     </div>
   );
